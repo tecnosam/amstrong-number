4763a74f5ef35da0c3372c63fe3eb9bbf8796322
tecnosam <49947131+tecnosam@users.noreply.github.com>
2020-06-15 20:14:27 +0100

    Update amstrong.cpp

--- amstrong.cpp
+++ amstrong.cpp
@@ -3,6 +3,25 @@
 #include <boost/lexical_cast.hpp>
 using namespace std;
 
+
+bool checkArmstrong(int n)
+{
+    int dig=log10(n)+1;	// Total number of digits of n
+    int res=0,x=n;
+    /* find sum of all the digits
+    raised to the power of the number of digits */  
+    while(n>0)
+    {
+        res+=pow(n%10,dig);
+        n/=10;
+    }
+    // If satisfies Armstrong condition
+    if(res==x)
+        return true;
+    else
+        return false;
+}
+
 bool check_amstrong(int &x, int &n) {
     int y = 0;
     // create s tring version of it for iteration
@@ -23,7 +42,7 @@ int main(){
     cin>>n;
     cout<<"running"<<endl;
     for ( int i = 10^(n-1); i < 10^n; i++ ) {
-        if ( check_amstrong(i, n) == true ) {
+        if ( checkArmstrong(i) == true ) {
             cout<<i<<endl;
         }
         else {
